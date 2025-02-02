2b1533642fa3a51b9c8d3d6df44884430e70a76f
Alexey <busienda@gmail.com>
2025-02-02 11:56:03 +0300

    src/Main.java added

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // Объявляем переменные
         int amount = 13_676; // Стоимость билета
-        int bonusAmount = 20; // Количество рублей для одной бонусной миль
+        int bonusAmount = 20; // Количество рублей для одной бонусной мили
 
         // Рассчитываем количество бонусных миль
         int bonus = amount / bonusAmount;
 
         // Выводим результат
-        System.out.println("Количество бонусных рублей: " + bonus);
+        System.out.println("Количество бонусных миль: " + bonus);
     }
 }
